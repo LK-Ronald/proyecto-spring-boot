81518ffdfd17486f5b15b5cb5535283689a7f6b3
LkRonald <rpereirac@unicartagena.co>
2026-09-16 00:12:18 -0500

    se actualizaron las dependencias de todo el proyecto, para que pudieran compilar con la version de java 24

--- src/test/java/com/jcaa/usersmanagement/infrastructure/entrypoint/rest/controller/UserRestControllerTest.java
+++ src/test/java/com/jcaa/usersmanagement/infrastructure/entrypoint/rest/controller/UserRestControllerTest.java
@@ -28,7 +28,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -50,12 +50,12 @@ class UserRestControllerTest {
 
   @Autowired private MockMvc mockMvc;
 
-  @MockBean private CreateUserUseCase createUserUseCase;
-  @MockBean private UpdateUserUseCase updateUserUseCase;
-  @MockBean private DeleteUserUseCase deleteUserUseCase;
-  @MockBean private GetUserByIdUseCase getUserByIdUseCase;
-  @MockBean private GetAllUsersUseCase getAllUsersUseCase;
-  @MockBean private JwtTokenService jwtTokenService;
+  @MockitoBean private CreateUserUseCase createUserUseCase;
+  @MockitoBean private UpdateUserUseCase updateUserUseCase;
+  @MockitoBean private DeleteUserUseCase deleteUserUseCase;
+  @MockitoBean private GetUserByIdUseCase getUserByIdUseCase;
+  @MockitoBean private GetAllUsersUseCase getAllUsersUseCase;
+  @MockitoBean private JwtTokenService jwtTokenService;
 
   @Test
   @WithMockUser(roles = "REVIEWER")
